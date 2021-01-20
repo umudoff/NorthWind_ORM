3f8d7106a3c64459f62cf64b02886ccc656c8404
umudoff <u.amrah@gmail.com>
2021-01-20 17:14:19 +0400

    Added query to context.

---
+++ NorthWind_ORM/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace NorthWind_ORM.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<NorthWind_ORM.NorthWindContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(NorthWind_ORM.NorthWindContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
--- NorthWind_ORM/Program.cs
+++ NorthWind_ORM/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
+
 
 namespace NorthWind_ORM
 {
@@ -10,6 +12,38 @@ namespace NorthWind_ORM
     {
         static void Main(string[] args)
         {
+            int categoryId = 8;
+            var context = new NorthWindContext();
+
+
+            var orders = context.Orders.ToList();
+
+            foreach (var o in orders)
+            {
+                var od = o.Order_Details.Where(c=>c.Product.CategoryID == categoryId);
+                
+                foreach (var d in od)
+                {
+                  //  Console.WriteLine(o.OrderID+" "+o.Customer.ContactName+" "+ d.Product.ProductName+" " +d.Quantity+" "+d.UnitPrice+" " +d.Product.Category.CategoryName);
+                }
+            }
+
+
+            //Eager loading
+            var res = context.Orders.Include(od => od.Order_Details.Select(d => d.Product)).Include(c => c.Customer).
+                Where(o => o.Order_Details.All(p => p.Product.CategoryID == categoryId)).
+                Select(t=> new { t.Customer.ContactName , details= t.Order_Details.
+                         Select(n=> new { n.Product.ProductName, n.OrderID, n.UnitPrice, n.Quantity})}).ToList();
+  
+            foreach(var o in res)
+            {
+                foreach (var d in o.details)
+                {
+                    Console.WriteLine("Customer:"+o.ContactName+"; Product: "+d.ProductName+";  OrderID:"+d.OrderID+"; Quantity:"+d.Quantity+"; UnitPrice: "+d.UnitPrice);
+                }
+            }
+          
+            Console.ReadLine();
         }
     }
 }
